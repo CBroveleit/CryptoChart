e72174f12f1cabc9bee8303fc2257f9bee42b296
Chris Broveleit <Chris.Broveleit@gmail.com>
2019-05-09 14:18:21 -0700

    skeleton built

---
+++ client/components/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+const axios = require ('axios');
+
+import {
+    //CSS Styles imported here
+
+} from './styles'
+
+class Main extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+
+    render () {
+        //deconstruct here
+        return(
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+}
